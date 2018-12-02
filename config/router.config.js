fe3326618c659b41e12677f78cbb14d7b71543b4
YooHoeh <zako2810@sina.com>
2018-12-02 12:39:20 +0800

    init

---
+++ config/router.config.js
@@ -0,0 +1,53 @@
+export default [
+  // user
+  {
+    path: '/user',
+    component: '../layouts/UserLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', component: './User/Login' },
+      { path: '/user/register', component: './User/Register' },
+      { path: '/user/register-result', component: './User/RegisterResult' },
+    ],
+  },
+  // app
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    Routes: ['src/pages/Authorized'],
+    authority: ['admin', 'user', 'guest'],
+    routes: [
+      { path: '/', redirect: '/dashboard' },
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        icon: 'dashboard',
+        component: './Dashboard/Analysis',
+      },
+
+      {
+        name: 'People',
+        icon: 'team',
+        path: '/people',
+      },
+      {
+        name: 'Projects',
+        icon: 'gold',
+        path: '/projects',
+      },
+      {
+        name: 'Publications',
+        icon: 'book',
+        path: '/publications',
+      },
+      {
+        name: 'Contact Us',
+        icon: 'message',
+        path: '/contact',
+      },
+      {
+        component: '404',
+      },
+    ],
+  },
+];
---
+++ src/defaultSettings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  navTheme: 'dark',
+  primaryColor: '#2F54EB',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
+  fixedHeader: true,
+  autoHideHeader: true,
+  fixSiderbar: false,
+};
